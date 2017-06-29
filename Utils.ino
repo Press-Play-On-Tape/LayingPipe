ae09168a59e3dd59ae8cd24d7ec1c43b11dfa45b
filmote <simon@bloggingadeadhorse.com>
2017-06-29 11:24:51 +1000

    Initial commit.

--- Utils.ino
+++ Utils.ino
@@ -1,19 +1,8 @@
 bool nodeAlreadyPlayed(byte value) { 
-//Serial.println("nodeAlreadyPlayed()");
+
     for (byte y = 0; y < maze.maxY; y++) {
       
       for (byte x = 0; x < maze.maxX; x++) {
-/*
-Serial.print(" x: ");
-Serial.print(x);
-Serial.print(", y: ");
-Serial.println(y);
-Serial.print(" getNodeValue(x, y): ");
-Serial.println(getNodeValue(x, y));
-Serial.print(" isNode(x, y): ");
-Serial.println(isNode(x, y));
-Serial.println(" ");
- */       
         if (getNodeValue(x, y) == value && !isNode(x, y)) {
           
           return true;
@@ -22,14 +11,14 @@ Serial.println(" ");
         
     }
       
-    }
+  }
     
-    return false;
+  return false;
     
 }
 
 bool clearBoard(byte nodeValue) {
-Serial.println("clearBoard()");
+
   for (byte y = 0; y < maze.maxY; y++) {
     
     for (byte x = 0; x < maze.maxX; x++) {
@@ -40,7 +29,7 @@ Serial.println("clearBoard()");
         
       }
       
-  }
+    }
     
   }
   
@@ -85,34 +74,22 @@ void setPipeValue(byte x, byte y, byte pipeValue, byte nodeValue) {
 
 bool validMove(byte direction, SelectedNode selectedNode, byte x, byte y) {
 
-
-   Serial.println("validMove()");
-
+	
   // Off the grid!
+
   if (x < 0 || x >= maze.maxX || y < 0 || y >= maze.maxY) return false;
-    Serial.println("  validMove 2");
-    Serial.print("    x: ");
-    Serial.println(x);
-    Serial.print("    y: ");
-    Serial.println(y);
-    Serial.print("    selectedNode: ");
-    Serial.println(selectedNode.value);
-    Serial.print("    isNode(x,y): ");
-    Serial.println(isNode(x,y));
-    Serial.print("    getPipeValue(x,y): ");
-    Serial.println(getPipeValue(x,y));
-    Serial.print("    getNodeValue(x,y): ");
-    Serial.println(getNodeValue(x,y));
   
+  
+  // Is it a clear cell or the matching node?
   
   if (
       (!isNode(x,y) && getPipeValue(x,y) == NOTHING) ||
       (isNode(x,y) && getNodeValue(x,y) == selectedNode.value && (x != selectedNode.x || y != selectedNode.y))
      ) return true;
   
-   Serial.println("  validMove 3");
 
   // Is the pipe turning back on itself?
+
   switch (direction) {
 
     case (UP):
@@ -123,9 +100,6 @@ bool validMove(byte direction, SelectedNode selectedNode, byte x, byte y) {
         case PIPE_CORNER_RB:
         case PIPE_CORNER_LB:
           return true;
-
-        default:
-          return false;
 
       }
 
@@ -140,9 +114,6 @@ bool validMove(byte direction, SelectedNode selectedNode, byte x, byte y) {
         case PIPE_CORNER_RT:
           return true;
 
-        default:
-          return false;
-
       }
 
       break;
@@ -156,9 +127,6 @@ bool validMove(byte direction, SelectedNode selectedNode, byte x, byte y) {
         case PIPE_HORIZONTAL_LR:
           return true;
 
-        default:
-          return false;
-
       }
 
       break;
@@ -171,9 +139,6 @@ bool validMove(byte direction, SelectedNode selectedNode, byte x, byte y) {
         case PIPE_CORNER_BL:
         case PIPE_HORIZONTAL_RL:
           return true;
-
-        default:
-          return false;
 
       }
 
@@ -190,25 +155,25 @@ bool validMove(byte direction, SelectedNode selectedNode, byte x, byte y) {
  *  
  *  So much nicer than a solid line!
  *  
- */
+ * /
 void drawHorizontalLine(byte x1, byte x2, byte y) {
 
   for (byte x3 = x1; x3 <= x2; x3+=2) {
     arduboy.drawPixel(x3, y, WHITE);
   }
   
-}
+}*/
 
 /* ----------------------------------------------------------------------------
  *  Draw a horizontal dotted line. 
  *  
  *  So much nicer than a solid line!
  *  
- */
+ * /
 void drawVerticalLine(byte x, byte y1, byte y2) {
 
   for (byte y3 = y1; y3 <= y2; y3+=2) {
     arduboy.drawPixel(x, y3, WHITE);
   }
   
-}
+}*/
