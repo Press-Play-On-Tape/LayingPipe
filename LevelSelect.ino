f2c1ac36050087ee0dab88869877eda2aaa36689
Simon Holmes <simon@bloggingadeadhorse.com>
2017-07-01 23:40:28 +1000

    Initial commit.

--- LevelSelect.ino
+++ LevelSelect.ino
@@ -1,27 +1,64 @@
 
-#define MENU_ITEM_1_X          50
-#define MENU_ITEM_1_Y          12
-#define MENU_ITEM_2_X          MENU_ITEM_1_X
-#define MENU_ITEM_2_Y          24
-#define MENU_ITEM_3_X          MENU_ITEM_1_X
-#define MENU_ITEM_3_Y          26
-#define MENU_ITEM_4_X          MENU_ITEM_1_X
-#define MENU_ITEM_4_Y          48
-#define MENU_ITEM_WIDTH        60
-#define MENU_ITEM_HEIGHT       16
+#define MENU_ITEM_3_X          19
+#define MENU_ITEM_3_1_Y        19
+#define MENU_ITEM_3_2_Y        29
+#define MENU_ITEM_3_3_Y        39
+#define MENU_ITEM_3_WIDTH      92   
+#define MENU_ITEM_3_HEIGHT     9
+
+#define MENU_ITEM_2_X          17
+#define MENU_ITEM_2_1_Y        23
+#define MENU_ITEM_2_2_Y        35
+#define MENU_ITEM_2_WIDTH      98   
+#define MENU_ITEM_2_HEIGHT     11
+
 
 byte levelSelect_selectedItem = 0;
 byte levelSelect_topItem = 0;
-byte levelSelect_noOfItem = 4;
+byte levelSelect_noOfItem = 3;
 byte puzzleSelect_selectedItem = 0;
 
 void levelSelect() {
 
   arduboy.clear();
   
-  if (arduboy.justPressed(UP_BUTTON) && levelSelect_selectedItem > 0) 					        { levelSelect_selectedItem--; }
-  if (arduboy.justPressed(DOWN_BUTTON) && levelSelect_selectedItem < sizeof(levels)) 	  { levelSelect_selectedItem++; }
-  if (arduboy.justPressed(B_BUTTON)) 												                            { gameState = STATE_GAME_INTRO; }
+  sprites.drawOverwrite(-2, -2, logo_elbow_RB, frame);
+  sprites.drawOverwrite(14, -2, logo_straight_LR, frame);
+  sprites.drawOverwrite(30, -2, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(46, -2, logo_straight_LR, frame);
+  sprites.drawOverwrite(62, -2, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(78, -2, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(80, -2, logo_straight_LR, frame);
+  sprites.drawOverwrite(84, -2, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(100, -2, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(114, -2, logo_elbow_LB, frame);
+
+  sprites.drawOverwrite(-2, 14, logo_straight_TB, frame);
+  sprites.drawOverwrite(-2, 24, logo_straight_TB_noflange, frame);
+  sprites.drawOverwrite(-2, 17, logo_straight_TB, frame);
+  sprites.drawOverwrite(-2, 36, logo_straight_TB, frame);
+
+  sprites.drawOverwrite(114, 14, logo_straight_TB, frame);
+  sprites.drawOverwrite(114, 24, logo_straight_TB_noflange, frame);
+  sprites.drawOverwrite(114, 17, logo_straight_TB, frame);
+  sprites.drawOverwrite(114, 36, logo_straight_TB, frame);
+
+  sprites.drawOverwrite(-2, 52, logo_elbow_TR, frame);
+  sprites.drawOverwrite(14, 52, logo_straight_LR, frame);
+  sprites.drawOverwrite(30, 52, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(46, 52, logo_straight_LR, frame);
+  sprites.drawOverwrite(62, 52, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(78, 52, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(80, 52, logo_straight_LR, frame);
+  sprites.drawOverwrite(84, 52, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(100, 52, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(114, 52, logo_elbow_TL, frame);
+
+
+  
+  if (arduboy.justPressed(UP_BUTTON) && levelSelect_selectedItem > 0) 					            { levelSelect_selectedItem--; }
+  if (arduboy.justPressed(DOWN_BUTTON) && levelSelect_selectedItem < sizeof(levels) - 1) 	  { levelSelect_selectedItem++; }
+  if (arduboy.justPressed(B_BUTTON)) 												                                { gameState = STATE_GAME_INTRO; }
   
   if (arduboy.justPressed(A_BUTTON)) { 
 	  
@@ -34,21 +71,26 @@ void levelSelect() {
   
   // Adjust the top menu item based on selected item ..
   
-  if (levelSelect_selectedItem < levelSelect_noOfItem) {
+  if (levelSelect_selectedItem < levelSelect_noOfItem - 1) {
 
 	  levelSelect_topItem = 0;
 
   }
   else {
 
-    if (levelSelect_selectedItem > sizeof(levels) - levelSelect_noOfItem) {
+    if (levelSelect_selectedItem > sizeof(levels) - levelSelect_noOfItem + 1) {
+Serial.println("b");
+Serial.print(sizeof(levels) );
+Serial.print(" ");
+Serial.print(levelSelect_noOfItem);
 
-      levelSelect_topItem = sizeof(levels) - levelSelect_noOfItem - 1;
+      levelSelect_topItem = sizeof(levels) - levelSelect_noOfItem;
 
   	}
   	else {
+Serial.println("c");
    
-      levelSelect_topItem = levelSelect_selectedItem - levelSelect_noOfItem + 1;
+      levelSelect_topItem = levelSelect_selectedItem - 1;
           
   	}
 
@@ -57,10 +99,9 @@ void levelSelect() {
   
   // Render menu ..
   
-  renderLevelDetail(MENU_ITEM_1_X, MENU_ITEM_1_Y, levelSelect_topItem, (levelSelect_topItem == levelSelect_selectedItem));
-  renderLevelDetail(MENU_ITEM_2_X, MENU_ITEM_2_Y, levelSelect_topItem, (levelSelect_topItem + 1 == levelSelect_selectedItem));
-  renderLevelDetail(MENU_ITEM_3_X, MENU_ITEM_3_Y, levelSelect_topItem, (levelSelect_topItem + 2 == levelSelect_selectedItem));
-  renderLevelDetail(MENU_ITEM_4_X, MENU_ITEM_4_Y, levelSelect_topItem, (levelSelect_topItem + 3 == levelSelect_selectedItem));
+  renderLevelDetail(MENU_ITEM_3_X, MENU_ITEM_3_1_Y, levels[levelSelect_topItem], (levelSelect_topItem == levelSelect_selectedItem));
+  renderLevelDetail(MENU_ITEM_3_X, MENU_ITEM_3_2_Y, levels[levelSelect_topItem + 1], (levelSelect_topItem + 1 == levelSelect_selectedItem));
+  renderLevelDetail(MENU_ITEM_3_X, MENU_ITEM_3_3_Y, levels[levelSelect_topItem + 2], (levelSelect_topItem + 2 == levelSelect_selectedItem));
     
 }
 
@@ -68,49 +109,49 @@ void renderLevelDetail(byte x, byte y, byte level, byte highlight) {
 
   if (highlight) {
 	  
-	arduboy.setTextColor(BLACK);
-	arduboy.setTextBackground(WHITE);
-	arduboy.fillRect(x - 2, y - 2, MENU_ITEM_WIDTH, MENU_ITEM_HEIGHT, WHITE);
+  	arduboy.setTextColor(BLACK);
+  	arduboy.setTextBackground(WHITE);
+  	arduboy.fillRect(x - 1, y - 1, MENU_ITEM_3_WIDTH, MENU_ITEM_3_HEIGHT, WHITE);
 	  
   }
 	
-  arduboy.setCursor(MENU_ITEM_1_X, MENU_ITEM_1_Y);
+  arduboy.setCursor(x, y);
   
   switch (level) {
   
   	case PUZZLE_5X5:
   		arduboy.print("Practice  ");
   		arduboy.print(readEEPROM(PUZZLE_5X5));
-  		arduboy.print(" of ");
+  		arduboy.print("/");
   		arduboy.print(puzzles_5x5_count);
   		break;
   	  
   	case PUZZLE_6X6:
-  		arduboy.print("Easy     ");
+  		arduboy.print("Easy      ");
   		arduboy.print(readEEPROM(PUZZLE_6X6));
-  		arduboy.print(" of ");
+      arduboy.print("/");
   		arduboy.print(puzzles_6x6_count);
   		break;
   	  
   	case PUZZLE_7X7:
-  		arduboy.print("Medium   ");
+  		arduboy.print("Medium    ");
   		arduboy.print(readEEPROM(PUZZLE_7X7));
-  		arduboy.print(" of ");
+      arduboy.print("/");
   		arduboy.print(puzzles_7x7_count);
   		break;
   	  
   	case PUZZLE_8X8:
-  		arduboy.print("Hard     ");
+  		arduboy.print("Hard      ");
   		arduboy.print(readEEPROM(PUZZLE_8X8));
-  		arduboy.print(" of ");
+      arduboy.print("/");
   		arduboy.print(puzzles_8x8_count);
   		break;
   	  
   	case PUZZLE_9X9:
-  		arduboy.print("Extreme  ");
+  		arduboy.print("Extreme   ");
   		arduboy.print(readEEPROM(PUZZLE_9X9));
-  		arduboy.print(" of ");
-  		arduboy.print(PUZZLE_9X9);
+  		arduboy.print("/");
+  		arduboy.print(puzzles_9x9_count);
   		break;  		
   }
   
@@ -123,7 +164,39 @@ void renderLevelDetail(byte x, byte y, byte level, byte highlight) {
 void puzzleSelect() {
 
   arduboy.clear();
-  
+ 
+  sprites.drawOverwrite(-2, -2, logo_elbow_RB, frame);
+  sprites.drawOverwrite(14, -2, logo_straight_LR, frame);
+  sprites.drawOverwrite(30, -2, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(46, -2, logo_straight_LR, frame);
+  sprites.drawOverwrite(62, -2, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(78, -2, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(80, -2, logo_straight_LR, frame);
+  sprites.drawOverwrite(84, -2, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(100, -2, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(114, -2, logo_elbow_LB, frame);
+
+  sprites.drawOverwrite(-2, 14, logo_straight_TB, frame);
+  sprites.drawOverwrite(-2, 24, logo_straight_TB_noflange, frame);
+  sprites.drawOverwrite(-2, 17, logo_straight_TB, frame);
+  sprites.drawOverwrite(-2, 36, logo_straight_TB, frame);
+
+  sprites.drawOverwrite(114, 14, logo_straight_TB, frame);
+  sprites.drawOverwrite(114, 24, logo_straight_TB_noflange, frame);
+  sprites.drawOverwrite(114, 17, logo_straight_TB, frame);
+  sprites.drawOverwrite(114, 36, logo_straight_TB, frame);
+
+  sprites.drawOverwrite(-2, 52, logo_elbow_TR, frame);
+  sprites.drawOverwrite(14, 52, logo_straight_LR, frame);
+  sprites.drawOverwrite(30, 52, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(46, 52, logo_straight_LR, frame);
+  sprites.drawOverwrite(62, 52, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(78, 52, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(80, 52, logo_straight_LR, frame);
+  sprites.drawOverwrite(84, 52, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(100, 52, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(114, 52, logo_elbow_TL, frame);
+   
   if (arduboy.justPressed(UP_BUTTON) && puzzleSelect_selectedItem > 0)         { puzzleSelect_selectedItem--; }
   if (arduboy.justPressed(DOWN_BUTTON) && puzzleSelect_selectedItem < 1)       { puzzleSelect_selectedItem++; }
   if (arduboy.justPressed(B_BUTTON))                                           { gameState = STATE_GAME_LEVEL_SELECT; }
@@ -133,7 +206,8 @@ void puzzleSelect() {
 	  if (puzzleSelect_selectedItem == 1) {
 		  
 		  puzzleIdx = 0;
-		  
+      updateEEPROM(puzzleType, puzzleIdx);
+      
 	  }
 	  
 	  if (puzzleSelect_selectedItem == 1) {
@@ -150,8 +224,8 @@ void puzzleSelect() {
   
   // Render menu ..
   
-  renderPuzzleOption(MENU_ITEM_2_X, MENU_ITEM_2_Y, "Continue Playing", (puzzleSelect_selectedItem == 0));
-  renderPuzzleOption(MENU_ITEM_3_X, MENU_ITEM_3_Y, "  Reset Level", (puzzleSelect_selectedItem == 1));
+  renderPuzzleOption(MENU_ITEM_2_X, MENU_ITEM_2_1_Y, "Continue Playing", (puzzleSelect_selectedItem == 0));
+  renderPuzzleOption(MENU_ITEM_2_X, MENU_ITEM_2_2_Y, "  Reset Level", (puzzleSelect_selectedItem == 1));
     
 }
 
@@ -162,11 +236,11 @@ void renderPuzzleOption(byte x, byte y, String message, byte highlight) {
 	  
 	arduboy.setTextColor(BLACK);
 	arduboy.setTextBackground(WHITE);
-	arduboy.fillRect(x - 2, y - 2, MENU_ITEM_WIDTH, MENU_ITEM_HEIGHT, WHITE);
+	arduboy.fillRect(x - 1, y - 2, MENU_ITEM_2_WIDTH, MENU_ITEM_2_HEIGHT, WHITE);
 	  
   }
 	
-  arduboy.setCursor(MENU_ITEM_1_X, MENU_ITEM_1_Y);
+  arduboy.setCursor(x, y);
   arduboy.print(message);
   
   arduboy.setTextColor(WHITE);
--- Splash.ino
+++ Splash.ino
@@ -4,6 +4,9 @@ void drawSplash() {
 
   bool skipSplash = false;
   
+  arduboy.clear(); 
+  arduboy.display();
+    
   skipSplash = splashAnimation(112,  0, logo_straight_TB_noflange, frame, skipSplash);
   skipSplash = splashAnimation(112, 16, logo_elbow_TL, frame, skipSplash);
   skipSplash = splashAnimation(96,  16, logo_elbow_TR, frame, skipSplash);
@@ -16,7 +19,7 @@ void drawSplash() {
   skipSplash = splashAnimation(64,  32, logo_elbow_LB, frame, skipSplash);
   skipSplash = splashAnimation(64,  48, logo_straight_TB_noflange, frame, skipSplash);
 
-  skipSplash = splashDelay(300, skipSplash);
+  skipSplash = splashDelay(200, skipSplash);
 
   skipSplash = splashAnimation(0,    0, logo_straight_TB_noflange, frame, skipSplash);
   skipSplash = splashAnimation(0,   16, logo_straight_TB, frame, skipSplash);
@@ -31,7 +34,7 @@ void drawSplash() {
   skipSplash = splashAnimation(96,  32, logo_straight_LR, frame, skipSplash);
   skipSplash = splashAnimation(112, 32, logo_straight_LR_noflange, frame, skipSplash);
 
-  skipSplash = splashDelay(300, skipSplash);
+  skipSplash = splashDelay(200, skipSplash);
 
   skipSplash = splashAnimation(0,    0, logo_cross_underlap, frame, skipSplash);
   skipSplash = splashAnimation(16,   0, logo_elbow_LB, frame, skipSplash);
@@ -44,7 +47,19 @@ void drawSplash() {
   skipSplash = splashAnimation(80,  48, logo_straight_LR_noflange, frame, skipSplash);
   skipSplash = splashAnimation(96,  48, logo_elbow_LB, frame, skipSplash);
 
+  skipSplash = splashDelay(200, skipSplash);
+
+  arduboy.setTextSize(2);
+  arduboy.setCursor(22, 10);
+  arduboy.print("Laying");
+  arduboy.setCursor(50, 36);
+  arduboy.print("Pipe");
+  arduboy.display();
+  arduboy.setTextSize(1);
+
+
   skipSplash = splashDelay(1000, skipSplash);
+
   
   gameState = STATE_GAME_LEVEL_SELECT;
   
@@ -110,14 +125,86 @@ bool splashDelay(int delayLength, bool skip) {
 void drawGameOver() { 
   
   arduboy.clear();
-  arduboy.setCursor(12, 30);
-  arduboy.print("Congratulations !");
+      
+  sprites.drawOverwrite(112,  0, logo_straight_TB_noflange, frame);
+  sprites.drawOverwrite(112, 16, logo_elbow_TL, frame);
+  sprites.drawOverwrite(96,  16, logo_elbow_TR, frame);
+  sprites.drawOverwrite(96,   0, logo_elbow_LB, frame);
+  sprites.drawOverwrite(80,   0, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(64,   0, logo_straight_LR, frame);
+  sprites.drawOverwrite(48,   0, logo_elbow_RB, frame);
+  sprites.drawOverwrite(48,  16, logo_straight_TB_noflange, frame);
+  sprites.drawOverwrite(48,  32, logo_elbow_TR, frame);
+  sprites.drawOverwrite(64,  32, logo_elbow_LB, frame);
+  sprites.drawOverwrite(64,  48, logo_straight_TB_noflange, frame);
+
+  sprites.drawOverwrite(0,    0, logo_straight_TB_noflange, frame);
+  sprites.drawOverwrite(0,   16, logo_straight_TB, frame);
+  sprites.drawOverwrite(0,   32, logo_elbow_TR, frame);
+  sprites.drawOverwrite(16,  32, logo_straight_LR, frame);
+  sprites.drawOverwrite(32,  32, logo_elbow_TL, frame);
+  sprites.drawOverwrite(32,  16, logo_elbow_RB, frame);
+  sprites.drawOverwrite(48,  16, logo_cross_underlap_hflange, frame);
+  sprites.drawOverwrite(64,  16, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(80,  16, logo_elbow_LB, frame);
+  sprites.drawOverwrite(80,  32, logo_elbow_TR, frame);
+  sprites.drawOverwrite(96,  32, logo_straight_LR, frame);
+  sprites.drawOverwrite(112, 32, logo_straight_LR_noflange, frame);
+
+  sprites.drawOverwrite(0,    0, logo_cross_underlap, frame);
+  sprites.drawOverwrite(16,   0, logo_elbow_LB, frame);
+  sprites.drawOverwrite(16,  16, logo_straight_TB, frame);
+  sprites.drawOverwrite(16,  32, logo_cross_overlap_hflange, frame);
+  sprites.drawOverwrite(16,  48, logo_elbow_TR, frame);
+  sprites.drawOverwrite(32,  48, logo_straight_LR, frame);
+  sprites.drawOverwrite(48,  48, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(64,  48, logo_cross_underlap_hflange, frame);
+  sprites.drawOverwrite(80,  48, logo_straight_LR_noflange, frame);
+  sprites.drawOverwrite(96,  48, logo_elbow_LB, frame);
   arduboy.display();
 
-  playPuzzleSolved();
-  delay(2000);
+  delay(1000);
+
+   arduboy.setTextColor(BLACK);
+    arduboy.setTextBackground(WHITE);
+
+
+  arduboy.fillRect(12, 23, 104, 18, WHITE);
+  arduboy.setCursor(18, 28);
+  arduboy.print("Congratulations!");
+  arduboy.display();
+
+  arduboy.setTextColor(WHITE);
+  arduboy.setTextBackground(BLACK);
+
+
 
   puzzleIdx++;
+  updateEEPROM(puzzleType, puzzleIdx);
+
+  playPuzzleSolved();
+  gameOverDelay(500, false);
+
   gameState = STATE_GAME_INIT_GAME;
   
 }
+
+bool gameOverDelay(int delayLength, bool skip) {
+
+  if (skip) return true;
+
+  arduboy.pollButtons();
+
+  while (delayLength >= 0) {
+
+    if (arduboy.justPressed(A_BUTTON) || arduboy.justPressed(B_BUTTON)) { return true; }
+    
+    arduboy.delayShort(5);  
+    delayLength--;
+
+  }
+
+  return false;
+  
+}
+
